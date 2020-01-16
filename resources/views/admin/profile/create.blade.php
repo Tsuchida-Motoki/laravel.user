5d60e38be25b36f3faddf500b1579d33d73630d2
TsuchidaMotoki <water7clock@gmail.com>
2020-01-16 06:39:48 +0000

    first commit

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.profile')
+        @section('title', 'profile')
+        
+        @section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>profile</h2>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,13 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileControll@eredit');
 });
 // 課題1   "Routing"
 // 課題2　"決まった設定を複数行うことができる"
 // 課題3
 Route::get('http://xxxxxx.jp/XXX', 'AAAController@bbb');
 // 課題4
-Route::get('admin/profile/create', 'admin/ProfileController@add');
-Route::get('admin/profile/edit', 'admin/ProfileControll@eredit');
+
 ?>
