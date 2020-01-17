201beb252b563fc24c08017777555a455810adb4
TsuchidaMotoki <water7clock@gmail.com>
2020-01-17 07:37:14 +0000

    first commit

--- resources/views/layouts/profile.blade.php
+++ resources/views/layouts/profile.blade.php
@@ -28,11 +28,30 @@
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <!-- Left Side Of Navbar -->
                         <ul class="navbar-nav mr-auto">
-
+                            @guest
+               <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
                         </ul>
+                             @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                            @endguest
+                            </ul>
                         <!-- Right Side Of Navbar -->
                         <ul class="navbar-nav ml-auto">
+                            <ul class="navbar-nav ml-auto">
                         </ul>
                     </div>
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,16 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit', 'Admin\ProfileControll@eredit');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
+    Route::get('profile/edit', 'Admin\ProfileControll@eredit')->middleware('auth');
+    
 });
-// 課題1   "Routing"
-// 課題2　"決まった設定を複数行うことができる"
-// 課題3
-Route::get('http://xxxxxx.jp/XXX', 'AAAController@bbb');
-// 課題4
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
 
 ?>
+<!--Auth::routes();-->
+
+<!--Route::get('/home', 'HomeController@index')->name('home');-->
