afa16001dfc86d8bbec56ec268d4fc181ecf47ac
TsuchidaMotoki <water7clock@gmail.com>
2020-02-06 04:47:26 +0000

    18課題

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -2,45 +2,20 @@
 
 @section('content')
     <div class="container">
-        <hr color="#c0c0c0">
-        @if (!is_null($headline))
-            <div class="row">
-                <div class="headline col-md-10 mx-auto">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <div class="caption mx-auto">
-                                <div class="image">
-                                    @if ($headline->image_path)
-                                        <img src="{{ asset('storage/image/' . $headline->image_path) }}">
-                                    @endif
-                                </div>
-                                <div class="title p-2">
-                                    <h1>{{ str_limit($headline->title, 70) }}</h1>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <p class="body mx-auto">{{ str_limit($headline->body, 650) }}</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif
+        
         <hr color="#c0c0c0">
         <div class="row">
             <div class="posts col-md-8 mx-auto mt-3">
-                @foreach($posts as $post)
+               
                     <div class="post">
                         <div class="row">
-                            <div class="text col-md-6">
-                                <div class="date">
-                                    {{ $post->updated_at->format('Y年m月d日') }}
-                                </div>
-                                <table class="table table-dark">
+                            <div class="text col-md-10">
+                                
+                        <table class="table table-dark">
                           <thead>
                           <tr>
                             <th width="10%">ID</th>
-                            <th widti="20%">氏名</th>
+                            <th width="20%">氏名</th>
                             <th width="20%">性別</th>
                             <th width="20%">趣味</th>
                             <th width="20%">自己紹介</th>
@@ -49,12 +24,12 @@
                     　     <tbody>
                     　    @foreach($posts as $profile)
                     　    <tr>
-                    　   <th>{{ $profile->id }}</th>
-                    　   <td>{{ \Str::limit($profile->title, 100)}}</td>
-                    　   <td>{{ \Str::limit($profile->body, 250)}}</td>
-                    　   <td>
                     　   
-                    　 </td>
+                    　   <td>{{ \Str::limit($profile->id, 20)}}</td>
+                    　   <td>{{ \Str::limit($profile->name, 30)}}</td>
+                    　   <td>{{ \Str::limit($profile->gender, 30)}}</td>
+                    　   <td>{{ \Str::limit($profile->hobby, 30)}}</td>
+                    　   <td>{{ \Str::limit($profile->introduction, 60)}}</td>
                      </tr>
                     　    @endforeach
                     　</tbody>
@@ -62,7 +37,7 @@
                         </div>
                     </div>
                     <hr color="#c0c0c0">
-                @endforeach
+                
             </div>
         </div>
     </div>
